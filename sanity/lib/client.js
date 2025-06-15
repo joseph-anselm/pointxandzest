a724c5048ed124b3c27875bb02dc82b59e346c0a
joseph-anselm <iamjoeanselm@gmail.com>
2025-06-16 00:04:17 +0100

    backend added

--- sanity/lib/client.js
+++ sanity/lib/client.js
@@ -1,10 +1,8 @@
-
-import { createClient } from "next-sanity";
+import { createClient } from '@sanity/client';
 
 export const client = createClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
-  apiVersion: '2023-05-03', // Use current date in YYYY-MM-DD format
-  useCdn: process.env.NODE_ENV === 'production', // Enable CDN in production
-  token: process.env.SANITY_API_TOKEN, // Only if you need write access
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
+  useCdn: true,
+  apiVersion: '2023-01-01',
 });
